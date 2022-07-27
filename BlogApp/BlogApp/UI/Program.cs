9df50a501d8fb67e56835946b9e1f78c83fb33ec
Emmaa20 <elmirafk@code.edu.az>
2022-07-27 14:53:30 +0400

    initial commit

---
+++ BlogApp/BlogApp/UI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BlogApp.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Available commands:");
+            Console.WriteLine();
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+            Console.WriteLine("/exit");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Pleace enter command :");
+                string command = Console.ReadLine();
+                if (command == "/register")
+                {
+                    //Authentication.Register();
+
+                }
+                else if (command == "/login")
+                {
+
+                    //Authentication.Login();
+
+                }
+                else if (command == "/exit")
+                {
+                    Console.WriteLine("Thanks for using our application");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Command not found ...");
+                }
+
+            }
+        }
+    }
+}
+
+
